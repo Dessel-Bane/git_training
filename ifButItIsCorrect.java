3b23e345f0736e730f640cbda920f474f3706ed8
sjo <vladimir.vorozhishchev.7e4@itb.cat>
2020-11-05 10:30:05 +0100

    Adding new program bruh

---
+++ ifButItIsCorrect.java
@@ -0,0 +1,10 @@
+public class ifButItIsCorrect{
+	public static void main(String[] args){
+		boolean a=true;
+		if (a==true){
+			System.out.println("Well, ok, now what dude?");
+		}else {
+			System.out.println("There's no other option, wtf, how did you get here?!");
+		}
+	}
+}
